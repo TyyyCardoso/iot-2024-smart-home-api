6513475db222c14ade0024083597f9da5f0ddf5f
Tiago Ourique Cardoso <86069519+TyyyCardoso@users.noreply.github.com>
2024-05-23 19:25:36 +0100

    Criado esqueleto do projeto para inicio de desenvolvimento

---
+++ src/main/java/smarthomeapi/SmartHomeApiApplication.java
@@ -0,0 +1,13 @@
+package smarthomeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHomeApiApplication.class, args);
+    }
+
+}
